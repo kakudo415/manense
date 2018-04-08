81c45ee11912a6052f42c8d4bcc128c1b55c8172
Kakudo <kakudo415@gmail.com>
2018-04-08 17:45:39 +0900

    Reset

--- src/common/common.go
+++
@@ -1,9 +0,0 @@
-package common
-
-// Common return common information
-func Common() map[string]string {
-	var c = map[string]string{}
-	c["ServiceName"] = "manense"
-	c["ServiceURL"] = "http://localhost:8000"
-	return c
-}
