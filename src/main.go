de8772ae50f30ceaf01795f990421e77c3bc143c
Kakudo <kakudo415@gmail.com>
2018-05-10 22:56:16 +0900

    Design

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -9,7 +8,6 @@ import (
 )
 
 func main() {
-	go REPL()
 	http.HandleFunc("/", page.Home)
 	http.HandleFunc("/new", page.New)
 	http.HandleFunc("/update", page.Update)
@@ -20,28 +18,4 @@ func main() {
 	http.HandleFunc("/signout", page.Signout)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(os.Getenv("MANENSE_ADDRESS"), nil)
-}
-
-// REPL func
-func REPL() {
-	var c string
-	sucMessage("SERVER START")
-	for {
-		fmt.Print(">> ")
-		fmt.Scan(&c)
-		switch c {
-		case "EXIT":
-			os.Exit(0)
-		default:
-			errMessage("UNKNOWN COMMAND")
-		}
-	}
-}
-
-func sucMessage(m string) {
-	fmt.Fprintf(os.Stdout, "\033[32m%s\033[0m\n", m)
-}
-
-func errMessage(m string) {
-	fmt.Fprintf(os.Stderr, "\033[31m%s\033[0m\n", m)
 }
