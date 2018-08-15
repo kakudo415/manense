15ab31f66447f8ce521037a61707bc95f39555ce
Kakudo <31888089+kakudo415@users.noreply.github.com>
2018-08-15 12:53:47 +0900

    Rewrite branch

--- src/orm/follow.go
+++
@@ -1,9 +0,0 @@
-package orm
-
-// IsFollow func
-func IsFollow(ui, oi string) bool {
-	var c int
-	var f = new(Follows)
-	Connect().Model(f).Where("sub_id = ?", ui).Where("obj_id = ?", oi).Count(&c).Close()
-	return c > 0
-}
