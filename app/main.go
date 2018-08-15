2f8e5c3a0f17b91c540e1543e7d1a060eae0168f
Kakudo <31888089+kakudo415@users.noreply.github.com>
2018-08-15 14:21:23 +0900

    gin を使うことに決定

--- app/main.go
+++ app/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"./page"
+	"github.com/gin-gonic/gin"
+)
+
+var app = gin.New()
+
+func main() {
+	app.GET("/", page.Index)
+	app.Run("127.0.0.1:8000")
+}
+
+func init() {
+	app.LoadHTMLGlob("view/*.html")
+}
---
+++ app/page/page.go
@@ -0,0 +1,9 @@
+package page
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Index page
+func Index(g *gin.Context) {
+}
